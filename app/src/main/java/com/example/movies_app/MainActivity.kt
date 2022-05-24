60a6687a88462fb71a17dee83b764b5dc7586c17
Vladimir <rva2007@yandex.com>
2022-05-25 00:36:51 +0700

    Lesson-1

--- app/src/main/java/com/example/movies_app/MainActivity.kt
+++ app/src/main/java/com/example/movies_app/MainActivity.kt
@@ -10,6 +10,9 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.example.movies_app.navigation.SetupNavHost
 import com.example.movies_app.ui.theme.Movies_AppTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +20,10 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             Movies_AppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                val navController = rememberNavController()
+                SetupNavHost(navController = navController)
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    Movies_AppTheme {
-        Greeting("Android")
-    }
-}
